41e208908beafaba3c70810d9d10d4a54cd7296d
amber <amber.chi@quectel.com>
2023-05-06 15:00:34 +0800

    Can auto quction and check APP

---
+++ AutoQuctionAndCheck/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace AutoQuctionAndCheck {
+    partial class Form1 {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.type = new System.Windows.Forms.GroupBox();
+            this.multi_radio = new System.Windows.Forms.RadioButton();
+            this.radio_100 = new System.Windows.Forms.RadioButton();
+            this.radio_10 = new System.Windows.Forms.RadioButton();
+            this.AntoNext_check = new System.Windows.Forms.CheckBox();
+            this.quick_btn = new System.Windows.Forms.Button();
+            this.slow_btn = new System.Windows.Forms.Button();
+            this.Next_btn = new System.Windows.Forms.Button();
+            this.AntoResult_Check = new System.Windows.Forms.CheckBox();
+            this.result_tbx = new System.Windows.Forms.TextBox();
+            this.quection_tbx = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.type.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // type
+            // 
+            this.type.Controls.Add(this.multi_radio);
+            this.type.Controls.Add(this.radio_100);
+            this.type.Controls.Add(this.radio_10);
+            this.type.Location = new System.Drawing.Point(14, 12);
+            this.type.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.type.Name = "type";
+            this.type.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.type.Size = new System.Drawing.Size(369, 81);
+            this.type.TabIndex = 0;
+            this.type.TabStop = false;
+            this.type.Text = "题目类型";
+            // 
+            // multi_radio
+            // 
+            this.multi_radio.AutoSize = true;
+            this.multi_radio.Location = new System.Drawing.Point(246, 21);
+            this.multi_radio.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.multi_radio.Name = "multi_radio";
+            this.multi_radio.Size = new System.Drawing.Size(49, 16);
+            this.multi_radio.TabIndex = 2;
+            this.multi_radio.Text = "乘法";
+            this.multi_radio.UseVisualStyleBackColor = true;
+            this.multi_radio.CheckedChanged += new System.EventHandler(this.multi_radio_CheckedChanged);
+            // 
+            // radio_100
+            // 
+            this.radio_100.AutoSize = true;
+            this.radio_100.Location = new System.Drawing.Point(127, 20);
+            this.radio_100.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.radio_100.Name = "radio_100";
+            this.radio_100.Size = new System.Drawing.Size(96, 16);
+            this.radio_100.TabIndex = 1;
+            this.radio_100.Text = "100内加减法";
+            this.radio_100.UseVisualStyleBackColor = true;
+            this.radio_100.CheckedChanged += new System.EventHandler(this.radio_100_CheckedChanged);
+            // 
+            // radio_10
+            // 
+            this.radio_10.AutoSize = true;
+            this.radio_10.Checked = true;
+            this.radio_10.Location = new System.Drawing.Point(8, 21);
+            this.radio_10.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.radio_10.Name = "radio_10";
+            this.radio_10.Size = new System.Drawing.Size(89, 16);
+            this.radio_10.TabIndex = 0;
+            this.radio_10.TabStop = true;
+            this.radio_10.Text = "10内加减法";
+            this.radio_10.UseVisualStyleBackColor = true;
+            this.radio_10.CheckedChanged += new System.EventHandler(this.radio_10_CheckedChanged);
+            // 
+            // AntoNext_check
+            // 
+            this.AntoNext_check.AutoSize = true;
+            this.AntoNext_check.Checked = true;
+            this.AntoNext_check.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.AntoNext_check.Location = new System.Drawing.Point(390, 22);
+            this.AntoNext_check.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.AntoNext_check.Name = "AntoNext_check";
+            this.AntoNext_check.Size = new System.Drawing.Size(115, 16);
+            this.AntoNext_check.TabIndex = 1;
+            this.AntoNext_check.Text = "自动显示下一题";
+            this.AntoNext_check.UseVisualStyleBackColor = true;
+            // 
+            // quick_btn
+            // 
+            this.quick_btn.Location = new System.Drawing.Point(524, 14);
+            this.quick_btn.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.quick_btn.Name = "quick_btn";
+            this.quick_btn.Size = new System.Drawing.Size(88, 23);
+            this.quick_btn.TabIndex = 2;
+            this.quick_btn.Text = "快些";
+            this.quick_btn.UseVisualStyleBackColor = true;
+            this.quick_btn.Click += new System.EventHandler(this.quick_btn_Click);
+            // 
+            // slow_btn
+            // 
+            this.slow_btn.Location = new System.Drawing.Point(635, 13);
+            this.slow_btn.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.slow_btn.Name = "slow_btn";
+            this.slow_btn.Size = new System.Drawing.Size(88, 23);
+            this.slow_btn.TabIndex = 3;
+            this.slow_btn.Text = "慢些";
+            this.slow_btn.UseVisualStyleBackColor = true;
+            this.slow_btn.Click += new System.EventHandler(this.slow_btn_Click);
+            // 
+            // Next_btn
+            // 
+            this.Next_btn.Location = new System.Drawing.Point(391, 61);
+            this.Next_btn.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Next_btn.Name = "Next_btn";
+            this.Next_btn.Size = new System.Drawing.Size(88, 23);
+            this.Next_btn.TabIndex = 4;
+            this.Next_btn.Text = "下一题";
+            this.Next_btn.UseVisualStyleBackColor = true;
+            this.Next_btn.Click += new System.EventHandler(this.Next_btn_Click);
+            // 
+            // AntoResult_Check
+            // 
+            this.AntoResult_Check.AutoSize = true;
+            this.AntoResult_Check.Checked = true;
+            this.AntoResult_Check.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.AntoResult_Check.Location = new System.Drawing.Point(524, 130);
+            this.AntoResult_Check.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.AntoResult_Check.Name = "AntoResult_Check";
+            this.AntoResult_Check.Size = new System.Drawing.Size(102, 16);
+            this.AntoResult_Check.TabIndex = 6;
+            this.AntoResult_Check.Text = "自动显示答案";
+            this.AntoResult_Check.UseVisualStyleBackColor = true;
+            this.AntoResult_Check.CheckedChanged += new System.EventHandler(this.AntoResult_Check_CheckedChanged);
+            // 
+            // result_tbx
+            // 
+            this.result_tbx.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.result_tbx.ForeColor = System.Drawing.Color.Black;
+            this.result_tbx.Location = new System.Drawing.Point(538, 190);
+            this.result_tbx.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.result_tbx.Name = "result_tbx";
+            this.result_tbx.Size = new System.Drawing.Size(132, 21);
+            this.result_tbx.TabIndex = 20;
+            // 
+            // quection_tbx
+            // 
+            this.quection_tbx.Location = new System.Drawing.Point(321, 190);
+            this.quection_tbx.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.quection_tbx.Name = "quection_tbx";
+            this.quection_tbx.Size = new System.Drawing.Size(157, 21);
+            this.quection_tbx.TabIndex = 21;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(503, 193);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(12, 12);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "=";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.quection_tbx);
+            this.Controls.Add(this.result_tbx);
+            this.Controls.Add(this.AntoResult_Check);
+            this.Controls.Add(this.Next_btn);
+            this.Controls.Add(this.slow_btn);
+            this.Controls.Add(this.quick_btn);
+            this.Controls.Add(this.AntoNext_check);
+            this.Controls.Add(this.type);
+            this.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.type.ResumeLayout(false);
+            this.type.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.GroupBox type;
+        private System.Windows.Forms.RadioButton multi_radio;
+        private System.Windows.Forms.RadioButton radio_100;
+        private System.Windows.Forms.RadioButton radio_10;
+        private System.Windows.Forms.CheckBox AntoNext_check;
+        private System.Windows.Forms.Button quick_btn;
+        private System.Windows.Forms.Button slow_btn;
+        private System.Windows.Forms.Button Next_btn;
+        private System.Windows.Forms.CheckBox AntoResult_Check;
+        private System.Windows.Forms.TextBox result_tbx;
+        private System.Windows.Forms.TextBox quection_tbx;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ AutoQuctionAndCheck/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoQuctionAndCheck {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+        int num1 = 10;//算术最大可取值
+        int rule_ = 1;//加减乘除法则
+        //自动出题
+        private void timer1_Tick(object sender, EventArgs e) {
+            //判断是否勾选了自动出题按钮,是则自动出题
+            if (AntoNext_check.Checked) {
+                string title = TitleInput();
+                if (AntoResult_Check.Checked) {
+                    ResultOutput(title);
+                    //result_tbx.Click += Result_tbx_Click;
+                } else {
+                    result_tbx.Text = "";
+                    while(AntoResult_Check.Checked){
+                        if (Next_btn.Enabled) {
+                            break;
+                        }
+                        ResultOutput(title);
+                       // result_tbx.Click += Result_tbx_Click;
+                    }
+                }
+
+            }
+        }
+
+       
+
+        //选择10内的加减算法
+        private void radio_10_CheckedChanged(object sender, EventArgs e) {
+            //随机数最大可取10 法则为+-
+            num1 = 10;
+            rule_ = 1;
+        }
+        //选择100内的加减算法
+        private void radio_100_CheckedChanged(object sender, EventArgs e) {
+            num1 = 100;
+            rule_ = 1;
+        }
+        //选择乘法100内
+        private void multi_radio_CheckedChanged(object sender, EventArgs e) {
+            num1 = 100;
+            rule_ = 2;
+        }
+        //出题
+        string TitleInput() {
+            Random rnd = new Random();
+            int numA = rnd.Next(num1);
+            int numB = rnd.Next(num1);
+            int rul = rnd.Next(0, rule_);
+            string rule = "";
+            if (rul == 0) {
+                rule = "-";
+            } else if (rule_ == 1) {
+                rule = "+";
+            } 
+            if (multi_radio.Checked) {
+                    rule = "*";
+            }
+            string title = numA + rule + numB;
+            quection_tbx.Text = title;
+            return title;
+        }
+        int result_out = 0;
+        //出答案
+        void ResultOutput(string title) {
+            result_out = 0;
+            if (title.Contains("+")) {
+                string[] nums = title.Split('+');
+                result_out = int.Parse(nums[0]) + int.Parse(nums[1]);
+            } else if (title.Contains("-")) {
+                string[] nums = title.Split('-');
+                result_out = int.Parse(nums[0]) - int.Parse(nums[1]);
+            } else if (title.Contains("*")) {
+                string[] nums = title.Split('*');
+                result_out = int.Parse(nums[0]) * int.Parse(nums[1]);
+            }
+            if (result_tbx.Text==result_out.ToString()) {
+                result_tbx.ForeColor = Color.Green;
+            }else {
+                result_tbx.ForeColor = Color.Blue;
+            }
+            result_tbx.Text = result_out.ToString();
+        }
+
+        private void Result_tbx_Click(object sender, EventArgs e) {
+           TextBox box =sender as TextBox;
+            // throw new NotImplementedException();
+            if (box.Text != result_out.ToString()) {
+                box.ForeColor = Color.Red;
+            }
+            box.Text = result_out.ToString();
+        }
+
+        private void Next_btn_Click(object sender, EventArgs e) {
+            result_tbx.Text = "";
+            string title = TitleInput();
+            if (AntoResult_Check.Checked) {
+                ResultOutput(title);
+            } 
+            else {
+                result_tbx.Text = "";
+                while (AntoResult_Check.Checked) {
+                    if (Next_btn.Enabled) {
+                        break;
+                    }
+                    ResultOutput(title);
+                }
+            }
+        }
+
+        private void quick_btn_Click(object sender, EventArgs e) {
+            timer1.Interval = 500;
+        }
+
+        private void slow_btn_Click(object sender, EventArgs e) {
+            timer1.Interval = 500 * 3;
+        }
+
+        //自动出答案按钮
+        private void AntoResult_Check_CheckedChanged(object sender, EventArgs e) {
+            if (!AntoResult_Check.Checked) {
+                result_tbx.Text = "";
+            }
+            else {
+                if (result_tbx.Text == result_out.ToString()) {
+                   
+                    result_tbx.ForeColor = Color.Green;
+                }
+                result_tbx.Text = result_out.ToString();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+    }
+}
